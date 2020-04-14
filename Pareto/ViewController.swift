ce4a4c5b48ae9ba1549512d521246773e93ba29a
Zulfa Aliyah <zulfaliyah22@gmail.com>
2020-04-15 00:16:31 +0700

    Initial Commit

---
+++ Pareto/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pareto
+//
+//  Created by Zulfa Aliyah on 15/04/20.
+//  Copyright © 2020 Zulfa Aliyah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
